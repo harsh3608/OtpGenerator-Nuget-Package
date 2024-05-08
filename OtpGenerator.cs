b33a93cf9505a807ec3d6fa04abbca313cc37c60
harsh3608 <harsh3608@gmail.com>
2024-05-08 09:59:31 +0530

    Add project files.

---
+++ OtpGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace OtpGenerator
+{
+    public static class OtpGenerator
+    {
+        public static string GenerateOTP(int otpLength)
+        {
+            //const int otpLength = 6; // Length of the OTP
+
+            // Generate a random OTP using a random number generator
+            var random = new Random();
+            var otp = string.Empty;
+
+            for (int i = 0; i < otpLength; i++)
+            {
+                otp += random.Next(0, 9); // Generate a random digit (0-9)
+            }
+
+            return otp;
+        }
+    }
+}
